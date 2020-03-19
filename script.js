e61965f0c58d3b2195d75e55be909cb9e1921da2
Maxim Liplyanin <liplyanin@yandex.ru>
2020-03-20 02:40:14 +0300

    fix: slider size

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+document.addEventListener('scroll',(event)=>{
+    let curPos = window.scrollY;
+    let blocks = document.querySelectorAll('body > div');
+    let links = document.querySelectorAll('#menu > a');
+
+    blocks.forEach(el=>{
+        if(el.offsetTop <= curPos && curPos<(el.offsetTop + el.offsetHeight)) {
+            links.forEach(a=>{
+                a.firstElementChild.classList.remove('active');                
+                if(a.getAttribute('href').substring(1)==el.getAttribute('id')){
+                    a.firstElementChild.classList.add('active')
+                }
+            })
+        }
+    })
+})
+
+
 let header = document.querySelector('.header');
 let menu = document.getElementById('menu');
 let mobileMenu = document.querySelector('.header__content__mobile-menu');
@@ -17,21 +35,18 @@ header.addEventListener('click', (event)=>{
         event.target.style = 'transform: rotate(180deg); top: 0; left: 0;';
         mobileMenu.classList.add('mobile-menu__active');
         mobileDark.classList.add('mobile-menu__active');
-        console.log(mobileMenu.children)
-
     }else if(event.target.classList.value=='header__content__mobile-menu_icon'&&mobileMenu.classList.value=='header__content__mobile-menu mobile-menu__active'){
         event.target.style = 'transform: rotate(90deg); top: auto; left: 30px;';
         header.querySelectorAll('div').forEach(el=> el.classList.remove('mobile-menu__active'));
    
         
-    }
-    console.log(mobileMenu.classList.value=='header__content__mobile-menu mobile-menu__active')
-    
+    }    
 })
 
 
 
 let slider = document.querySelector('.slider');
+let mainSlider = document.querySelector('.slider__content');
 slider.addEventListener('click', (event)=>{
     let activeSlide = document.querySelector('.slide_active');
     let buttonLeft = document.getElementById('button_left');
@@ -42,27 +57,53 @@ slider.addEventListener('click', (event)=>{
     if(event.target==buttonLeft||event.target==buttonRight){
         activeSlide.classList.remove('slide_active');
         if(activeSlide==slide1){
+            slide2.style = 'opacity: 0;';
             slide2.classList.add('slide_active');
             slider.style = 'background-color: #648BF0';
             bottomLine.style = 'display: none';
-            
+            let opasity = 0;
+            let intervalAnim = setInterval(function(){
+                if(opasity==1.0000000000000002){
+                    clearInterval(intervalAnim);
+                } else {
+                    opasity+=0.05;
+                    slide2.style = 'opacity:' + opasity;
+                }
+            }, 15);            
         }else if(activeSlide==slide2){
+            slide1.style = 'opacity: 0;';
             slide1.classList.add('slide_active');
             slider.style = 'background-color: #f06c64';
             bottomLine.style = 'display: block';
+            
+            let opasity = 0;
+            let intervalAnim = setInterval(function(){
+                if(opasity==1.0000000000000002){
+                    clearInterval(intervalAnim);
+                } else {
+                    opasity+=0.05;
+                    slide1.style = 'opacity:' + opasity; 
+                }
+                
+            }, 15);
         }
     } 
-    if(event.target.alt == 'phone_vertical'||event.target.classList=='black_screen1'){
-        let blackScreen1 = document.querySelector('.black_screen1');
-       
-        (blackScreen1.style.cssText == 'display: block;')?  blackScreen1.style = 'display: none;' : blackScreen1.style = 'display: block;';
-    }
-    if(event.target.alt == 'phone_horizontal'||event.target.classList=='black_screen2'){
-        let blackScreen2 = document.querySelector('.black_screen2');
-       
-        (blackScreen2.style.cssText == 'display: block;')?  blackScreen2.style = 'display: none;' : blackScreen2.style = 'display: block;';
-    }
 
+
+    if(event.target.alt == 'phoneOn'){
+        event.target.style = 'display: none';
+        let blackVert = document.getElementById('phone_vertical_black');
+        let blackHor = document.getElementById('phone_horisontal_black');
+        if(event.target.id=='phone_vertical') blackVert.style = 'display: flex';
+        if(event.target.id=='phone_horisontal') blackHor.style = 'display: flex';
+    }else if(event.target.alt == 'phoneOff'){
+        event.target.style = 'display: none';
+        let vPhone = document.getElementById('phone_vertical');
+        let hPhone = document.getElementById('phone_horisontal');
+        if(event.target.id=='phone_vertical_black') vPhone.style = 'display: flex';
+        if(event.target.id=='phone_horisontal_black') hPhone.style = 'display: flex';
+    }
+  
 })
 
 let portfolio = document.querySelector('.portfolio__content');
@@ -105,11 +146,13 @@ let popup = document.querySelector('.popup');
 let submitBtn = document.getElementById('submit-btn');
 let closeBtn = document.getElementById('close-btn');
 
+let inputName = document.getElementById('input-name');
+let inputMail = document.getElementById('input-mail');
+let subject = document.getElementById('subject');
+let describe = document.getElementById('describe');
+
  submitBtn.addEventListener('click', (event)=>{
-    let inputName = document.getElementById('input-name');
-    let inputMail = document.getElementById('input-mail');
-    let subject = document.getElementById('subject');
-    let describe = document.getElementById('describe');
+ 
     if (inputName.validity.valid&&inputMail.validity.valid) {
         popup.classList.add('popup_active');   
         (document.getElementById('input-subject').value=='')? subject.innerText = 'Without subject  ' : 
@@ -122,6 +165,11 @@ let closeBtn = document.getElementById('close-btn');
 })
 closeBtn.addEventListener('click', ()=>{
     popup.classList.remove('popup_active'); 
+    inputName.value = '';
+    inputMail.value = '';
+    document.getElementById('input-subject').value = ' ';
+    document.getElementById('describe-area').value = ' ';
+    
 })
 
 
